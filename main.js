f91c9d39dd8c094651408c49a500ab237f88d794
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2015-11-20 15:58:35 -0800

    Lab satisfying minimum requirements

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 // wait for DOM to load before running JS
-$(function() {
-  
-  // your code here
+$(document).ready(function() {
 
+	var marker = "X";
+	
+	function changeMarker() {
+		if (marker === "X") {
+    	marker = "O";
+    } else {
+    	marker = "X";
+			}
+	}
+
+
+  $("div.col-xs-4").on("click", function handleClick(event) {
+    if ($(this).text() === "X" || $(this).text() === "O") {
+    	return;
+  	}
+    $(this).text(marker);
+    changeMarker();
+	});
+
+	$("button").on("click", function handleClick(event) {
+    $("div.col-xs-4").text("");
+  });
+  // your code here
 });
