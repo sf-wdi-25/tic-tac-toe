c1c24aea4301a80e05dbe5a3e8afb388544fbb3f
anonym0us3 <nathan.shayevich@gmail.com>
2015-11-21 19:44:12 -0800

    Added variable X's & O's functionality

--- main.js
+++ main.js
@@ -7,13 +7,32 @@ $('.btn').on("click", function handleClick() {
 	$('.box').empty();
 });
 
-
+var count = 0;
 $('.box').click(function userInput() {
-     $(this).html('<img src=http://i.imgur.com/tbtS98N.png>');
+     if (count % 2 === 0) {
+     	$(this).html('<img src=http://i.imgur.com/tbtS98N.png>');
+
+     } else {
+		$(this).html('<img src="http://i.imgur.com/17mhzBU.png">');     	
+     }
+     count++;
  });
 
 $('.box').click(function userInput() {
-     $(this).html('<img src="http://i.imgur.com/17mhzBU.png">');
+     
  });
-
 });
+
+
+//Noah's original code, that I tweaked to make work (shifting between X's and O's with each click)
+// var count = 0;
+// $(".box").click(function() {
+// 	if (count % 2 === 0) {
+// 		$(this).text("X");
+
+// 	} else if (count % 2 !== 0){
+// 		$(this).text("O");
+// 	}
+// count++;
+// });
+
