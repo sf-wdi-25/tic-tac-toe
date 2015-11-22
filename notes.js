b6319d341ef2ba1c854181e5968e40512fe4ae81
trebloc <ctse77@alum.berkeley.edu>
2015-11-22 15:50:17 -0800

    finished lab assignment

---
+++ notes.js
@@ -0,0 +1,18 @@
+// your code here
+  /*var turn = "o";
+
+  $('div.box').on('click',function handleClick() {
+
+  //function getTurn(f){
+    var playerO = "O";
+    var playerX = "X"; 
+  }
+
+  })*/
+/*
+  $('.box').click(function userInput() {
+    $(this).text("O");
+  });
+
+  if($(this).text() === "")
+*/  
