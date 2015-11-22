1b63d26c6c1251ad30ed582d6602c253b5e4e9c7
anonym0us3 <nathan.shayevich@gmail.com>
2015-11-22 13:10:31 -0800

    Game over!

--- main.js
+++ main.js
@@ -4,35 +4,23 @@ $(window).ready( function ( event ) {
   
   // your code here
 $('.btn').on("click", function handleClick() {
+	count = 0;
 	$('.box').empty();
 });
 
 var count = 0;
-$('.box').click(function userInput() {
-     if (count % 2 === 0) {
-     	$(this).html('<img src=http://i.imgur.com/tbtS98N.png>');
-
-     } else {
-		$(this).html('<img src="http://i.imgur.com/17mhzBU.png">');     	
-     }
-     count++;
- });
-
-$('.box').click(function userInput() {
-     
- });
+$('.box').click(function() {
+	if($(this).html() === "") {
+		if (count % 2 === 0) {
+			$(this).html('<img src=http://i.imgur.com/tbtS98N.png>');
+		} else {
+			$(this).html('<img src="http://i.imgur.com/17mhzBU.png">');     	
+     	}
+     	count++;
+     	console.log(count);
+    } else {
+    	console.log("can't let you do that StarFox");
+    }
 });
 
-
-//Noah's original code, that I tweaked to make work (shifting between X's and O's with each click)
-// var count = 0;
-// $(".box").click(function() {
-// 	if (count % 2 === 0) {
-// 		$(this).text("X");
-
-// 	} else if (count % 2 !== 0){
-// 		$(this).text("O");
-// 	}
-// count++;
-// });
-
+});
