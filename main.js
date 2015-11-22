c015b16313cca16bdd2b00f559a2f9bede3dcff3
anonym0us3 <nathan.shayevich@gmail.com>
2015-11-21 16:35:50 -0800

    With many Thanks to Colbert, got reset button up-&-running as well as onClick functionality

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 // wait for DOM to load before running JS
+console.log("Sanity Check = Eez Alive");
 $(window).ready( function ( event ) {
   
   // your code here
+$('.btn').on("click", function handleClick() {
+	$('.box').empty();
+});
 
 
+$('.box').click(function userInput() {
+     $(this).html('<img src=http://i.imgur.com/tbtS98N.png>');
+ });
+
+$('.box').click(function userInput() {
+     $(this).html('<img src="http://i.imgur.com/17mhzBU.png">');
+ });
 
 });
