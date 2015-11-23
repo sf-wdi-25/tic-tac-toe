f796e72dcd4afd3b004a58b09bb8dd7299d963ab
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2015-11-22 21:33:13 -0800

    completed assignment

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 $(document).ready(function() {
 
 	var marker = "X";
-	
-	function changeMarker() {
+
+	function switchMarker() {
 		if (marker === "X") {
     	marker = "O";
     } else {
@@ -11,17 +11,56 @@ $(document).ready(function() {
 			}
 	}
 
+	function switchMessage() {
+		$("p").text("It is player " + marker + "'s turn");
+	}
 
-  $("div.col-xs-4").on("click", function handleClick(event) {
+  function isThereWinner() {
+    if (($(".box").eq(0).text() === marker) && ($(".box").eq(1).text() === marker) && (($(".box").eq(2).text() === marker))) {
+      return true;
+      } else if (($(".box").eq(3).text() === marker) && ($(".box").eq(4).text() === marker) && (($(".box").eq(5).text() === marker))) {
+          return true;
+        } else if (($(".box").eq(6).text() === marker) && ($(".box").eq(7).text() === marker) && (($(".box").eq(8).text() === marker))) {
+            return true;
+          } else if (($(".box").eq(0).text() === marker) && ($(".box").eq(3).text() === marker) && (($(".box").eq(6).text() === marker))) {
+              return true;
+            } else if (($(".box").eq(1).text() === marker) && ($(".box").eq(4).text() === marker) && (($(".box").eq(7).text() === marker))) {
+                return true;
+              } else if (($(".box").eq(2).text() === marker) && ($(".box").eq(5).text() === marker) && (($(".box").eq(8).text() === marker))) {
+                  return true;
+                } else if (($(".box").eq(0).text() === marker) && ($(".box").eq(4).text() === marker) && (($(".box").eq(8).text() === marker))) {
+                    return true;
+                  } else if (($(".box").eq(2).text() === marker) && ($(".box").eq(4).text() === marker) && (($(".box").eq(6).text() === marker))) {
+                      return true;
+                    }
+  }
+
+  function reset() {
+    $("div.col-xs-4").text("");
+    marker = "X";
+  }
+
+  $("div.col-xs-4").on("click", function makeMove(event) {
     if ($(this).text() === "X" || $(this).text() === "O") {
     	return;
   	}
     $(this).text(marker);
-    changeMarker();
+    if ($(this).text() === "O") {
+      $(this).css("color", "blue");
+    } else if ($(this).text() === "X") {
+        $(this).css("color", "red");
+      }
+    if (isThereWinner()) {
+      alert("Winner is player " + marker);
+      reset();
+    } else {
+        switchMarker();
+        switchMessage();
+      }
 	});
 
-	$("button").on("click", function handleClick(event) {
+	$("button").on("click", function resetBoard(event) {
     $("div.col-xs-4").text("");
+    marker = "X";
   });
-  // your code here
 });
