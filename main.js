fd7f1e28e7a9858a4047ff98207ed932c55cf6fc
revalidate <reva.bhatt@gmail.com>
2015-11-23 09:52:54 -0800

    tic tac toe hw completed

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-// wait for DOM to load before running JS
-$(function() {
-  
-  // your code here
+console.log("Sanity Check: JS is working!");
+
+$(document).ready(function(){
+	var player = 0;
+	$(".box").click(function drawX() {
+		if ($(this).html() === "") {
+			if (player%2===0) {
+				($(this).html("<img src='img1.png'>"));
+			} else {
+				($(this).html("<img src='img2.png'>"));
+			}
+			player ++;
+		}
+	});
+	$("button").click(function reset() {
+		$(".box").text("");
+	});
+	$("button").mouseover(function changeHoverColor(){
+		$(this).css("background-color", "darkgoldenrod");
+	});
 
 });
